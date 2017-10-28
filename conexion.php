83578922268d24b1a60ab43b1757097ca8eca76f
JJCervera <jjcervera05@gmail.com>
2017-10-28 20:41:04 -0300

    variables conexion.php a BOP 22

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 // archivo único de configuración de conexión a la base de datos mysql
 $hostname="localhost";  
-$username="root";  
-$password="sistema";  
+$username="sistema";  
+$password="sistemabop22";  
 $db = "sistemadecontrol";  
 $dbh = new PDO("mysql:host=$hostname;dbname=$db", $username, $password);
 ?>
