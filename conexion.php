151e8e3f5e941e3e55f1968044625b9c7eec3175
JJCervera <jjcervera05@gmail.com>
2017-10-28 20:39:46 -0300

    conexion.php valores default

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 // archivo único de configuración de conexión a la base de datos mysql
 $hostname="localhost";  
-$username="sistema";  
-$password="sistemabop22";  
+$username="root";  
+$password="sistema";  
 $db = "sistemadecontrol";  
 $dbh = new PDO("mysql:host=$hostname;dbname=$db", $username, $password);
 ?>
